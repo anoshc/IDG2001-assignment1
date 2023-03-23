f356b1ec8e6c6260d7c657e08c1eea7a17b9e571
lexavan <alexaev@stud.ntnu.no>
2023-03-23 14:20:36 +0100

    fixfix

--- Backend/main.py
+++ Backend/main.py
@@ -7,10 +7,12 @@ import os
 # Import files
 import database
 import functions
+import vcard_parser
 
 # Imported functions from files
-from database import collection 
-from functions import 
+from database import collection
+from vcard_parser import vcard_parser
+# from functions import
 
 # Set the flask app
 app = Flask(__name__)
@@ -19,48 +21,45 @@ app = Flask(__name__)
 # POST /contacts endpoint
 @app.route('/contacts', methods=['POST'])
 def newContact():
-    user_data = export.json
+ vcard_parser()
+ with open('data.json') as file:
+    file_data = json.load(file)
+ if isinstance(file_data, list):
+    collection.insert_many(file_data) 
+ else:
+    collection.insert_one(file_data)
+   # user_data = export.json
     # implement code to create and return user data
-    return jsonify(user)
+    # return jsonify(user)
 
 
-# GET /contacts endpoint (json)
 
-
+    # GET /contacts endpoint (json)
+    @app.route('/contacts', methods=['GET'])
 # GET /contacts/id endpoint (json)
-
-
 # GET /contacts/vcard (vcard)
-
-
 # GET /contacts/id/vcard (vcard)
 
 
 
 
 
-
-
-''' TEST API FOR Å SJEKKE OM DET FUNKA BERRE'''
-@app.route('/railway', methods=['GET'])
-# Dette er controlleren til Endpointet
-def hello_railway():
-    data = collection.find()
-    return f'Hello, {data}!'
-
 # Hele greia er et Endpoint
 # Dette er routen til Endpointet
-@app.route('/', methods=['GET'])
+
+
+
+    @app.route('/', methods=['GET'])
 # Dette er controlleren til Endpointet
-def hello_world():
-    response = {'message': 'Hello from gruppen!'}
-    return jsonify(response)
+    def hello_world():
+     response = {'message': 'Hello from gruppen!'}
+     return jsonify(response)
 
 
 # Just a standard if that is needed in every flask application
 if __name__ == '__main__':
     app.run(port=4000)
-    #app.run(host=os.getenv('HOST', 'localhost'), port=os.getenv('PORT', '5000'))
+    # app.run(host=os.getenv('HOST', 'localhost'), port=os.getenv('PORT', '5000'))
 
 
 # Run app
---
+++ Backend/vcard_parser.py
@@ -0,0 +1,47 @@
+def vcard_parser():
+    import json
+
+    INPUT_NAME = 'vcard.vcf' # Må endre til at input er den fila vi laster opp
+    OUTPUT_NAME = 'data.json' # Json outout fila vi får
+
+    with open(INPUT_NAME, 'r') as f:
+        text = f.read()
+
+    contact_texts = text.split('END:VCARD\nBEGIN:VCARD')
+    contact_texts[0] = contact_texts[0].replace('BEGIN:VCARD\n', '')
+    contact_texts[-1] = contact_texts[-1].replace('END:VCARD\n', '')
+
+    def fix_key(key):
+        key_main = key.split(';')[0]
+        return {
+            'BDAY': 'birthday',
+            'VERSION': 'version',
+            'N': 'name',
+            'FN': 'first name',
+            'ORG': 'organisation',
+            'ADR': 'address',
+            'TEL': 'telefon',
+            'EMAIL': 'email'
+        }.get(key_main, key)
+    
+    contacts = []
+    for contact_text in contact_texts:
+        contact = {}
+        lines = contact_text.split('\n')
+
+    for line in lines:
+        if len(line) < 1:
+            continue
+
+        key, *value = line.split(':')
+        value = ':'.join(value)
+        
+        key = fix_key(key)
+
+        contact[key] = value
+    contacts.append(contact)   
+
+    json_text = json.dumps(contacts, indent=2)
+    
+    with open(OUTPUT_NAME, 'w') as f:
+        f.write(json_text)
